ca35016cf98a321f6eeee226c3e786effa93e9b6
Stevo <stevo@sunsetmagicwerks.com>
2020-11-05 16:13:01 -0800

    Reworked AnimationImageView to use AnimationTimer.

--- Source/View/AnimatedImageView.swift
+++ Source/View/AnimatedImageView.swift
@@ -9,67 +9,10 @@
 import SwiftUI
 
 //----------------------------------------------------------------------------------------------------------------------
-// MARK: FrameIndex
-private class FrameIndex : ObservableObject {
-
-	// MARK: Types
-	typealias CompletionProc = () -> Void
-
-	// MARK: Properties
-	@Published
-			var	value = 0
-
-	private	let	completionFrameIndex :Int
-
-	private	let	completionProc :CompletionProc
-
-	private	var	timer :Timer!
-
-	// MARK: Lifecycle methods
-	//------------------------------------------------------------------------------------------------------------------
-	init(framerate :Double, completionFrameIndex :Int, completionProc :@escaping CompletionProc) {
-		// Store
-		self.completionFrameIndex = completionFrameIndex
-
-		self.completionProc = completionProc
-
-		// Setup
-		self.timer =
-				Timer.scheduledTimer(withTimeInterval: 1.0 / framerate, repeats: true)
-					{ [unowned self] in
-						// Update value
-						self.value += 1
-
-						// Check value
-						if self.value == self.completionFrameIndex {
-							// Done
-							$0.invalidate()
-
-							// Call proc
-							self.completionProc()
-						}
-					}
-	}
-
-	//------------------------------------------------------------------------------------------------------------------
-	deinit {
-		self.timer.invalidate()
-	}
-}
-
-//----------------------------------------------------------------------------------------------------------------------
-// MARK: - AnimatedImageView
+// MARK: AnimatedImageView
 struct AnimatedImageView : View {
 
-	// MARK: Types
-	typealias CompletionProc = () -> Void
-
 	// MARK: Properties
-	private	let	images :[Image]
-
-	@ObservedObject
-	private	var	frameIndex :FrameIndex
-
 			var	body :some View {
 						GeometryReader { geometry in
 							self.images[self.frameIndex.value % self.images.count]
@@ -79,14 +22,20 @@ struct AnimatedImageView : View {
 						}
 					}
 
+	private	let	images :[Image]
+
+	@ObservedObject
+	private	var	animationTimer :AnimationTimer
+
 	// MARK: Lifecycle methods
 	//------------------------------------------------------------------------------------------------------------------
-	init(images :[Image], framerate :Double, completionProc :@escaping CompletionProc) {
+	init(images :[Image], framerate :Double, completionProc :@escaping () -> Void) {
 		// Store
 		self.images = images
 
 		// Setup
-		self.frameIndex =
-				FrameIndex(framerate: framerate, completionFrameIndex: images.count - 1, completionProc: completionProc)
+		self.animationTimer =
+				AnimationTimer(framerate: framerate, completionFrameIndex: images.count - 1,
+						completionProc: completionProc)
 	}
 }
